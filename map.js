b47b9c5a742e9943ab8c4ad7488f7a5aeffcffac
aaarf404 <alexmrf6439@gmail.com>
2025-05-16 18:59:23 -0700

    fix file structure

--- map.js
+++ map.js
@@ -15,3 +15,23 @@ const map = new mapboxgl.Map({
   minZoom: 5, // Minimum allowed zoom
   maxZoom: 18, // Maximum allowed zoom
 });
+
+map.on('load', async () => {
+    // Add the GeoJSON data source for Boston bike lanes
+    map.addSource('boston_route', {
+      type: 'geojson',
+      data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+    });
+  
+    map.addLayer({
+      id: 'bike-lanes',
+      type: 'line',
+      source: 'boston_route',
+      paint: {
+        'line-color':'#32D400',
+        'line-width': 3,
+        'line-opacity': 0.4,
+      },
+    });
+  });
+  
